0b6061a1723656d7e107c96a0e42573e4817df99
simulot <jfcassan+github@gmail.com>
2013-12-09 16:37:10 +0100

    Fix register timoout

--- device.go
+++ device.go
@@ -15,18 +15,18 @@ import (
 
 const (
 	EventTimeout    = 120 * time.Second // Give timout for HP GetEvent
-	RegisterTimeOut = 30 * time.Minute  // Give register interval
+	RegisterTimeOut = 60 * time.Minute  // Give register interval
 )
 
 type Device struct {
-	Name         string
-	URL          string
-	Destinations map[string]*Destination
-	ScanBatch    *ScanBatch
-	AgingStamp   string
-	events       chan HPEventTable
-	errors       chan error
-	register     <-chan time.Time
+	Name          string
+	URL           string
+	Destinations  map[string]*Destination
+	ScanBatch     *ScanBatch
+	AgingStamp    string
+	events        chan HPEventTable
+	errors        chan error
+	registerTimer *time.Timer
 }
 
 type deviceError struct {
@@ -81,29 +81,30 @@ func (d *Device) MainLoop() error {
 	defer Un(Trace("Device.MainLoop"))
 	var err error
 
+	var innerloopclose chan bool
 	d.events = make(chan HPEventTable)
 	d.errors = make(chan error)
 	err = d.Register(DefaultDestinationSettings)
 	if err == nil {
-		err = d.StartEventChannel()
+		innerloopclose, err = d.StartEventChannel()
 	}
 	if err == nil {
-		d.register = time.After(RegisterTimeOut)
+		d.registerTimer = time.NewTimer(RegisterTimeOut)
 		for {
 			select {
 			case Events := <-d.events:
 				TRACE.Println("Device.MainLoop", "Got events!")
 				err = d.ParseEventTable(&Events)
 				if err == nil {
-					d.register = time.After(RegisterTimeOut)
+					d.registerTimer.Reset(RegisterTimeOut)
 				}
-			case <-d.register:
+			case <-d.registerTimer.C:
 				// Timer is ser at each loop step.
 				TRACE.Println("Device.MainLoop", "Got timeout!")
-				err = d.Register(DefaultDestinationSettings)
+				innerloopclose <- true // Send a bullet to Event goroutine
+				d.registerTimer.Reset(RegisterTimeOut)
 				if err == nil {
-					err = d.StartEventChannel()
-					d.register = time.After(RegisterTimeOut)
+					innerloopclose, err = d.StartEventChannel()
 				}
 			case err = <-d.errors:
 				// Error!
@@ -119,8 +120,10 @@ func (d *Device) MainLoop() error {
 	return err
 }
 
-func (d *Device) StartEventChannel() error {
+func (d *Device) StartEventChannel() (chan bool, error) {
 	defer Un(Trace("Device.StartEventChannel"))
+
+	closeme := make(chan bool)
 
 	go func() {
 		defer Un(Trace("Device.StartEventChannel.StartGoRoutine"))
@@ -153,8 +156,16 @@ func (d *Device) StartEventChannel() error {
 		resp.Body.Close()
 		// Send firts events.
 		d.events <- *EventTable
-
 		for {
+			select {
+			case <-closeme:
+				// Sorry, I must dye
+				TRACE.Println("Device.StartEventChannel.goroutine", "closeme !")
+				return
+			default:
+				//Dont block the loop
+			}
+
 			headers["If-None-Match"] = Etag
 			resp, err := HttpGetWithTimeout(d.URL+"/EventMgmt/EventTable?timeout="+fmt.Sprintf("%d", int(EventTimeout.Seconds())*10), &headers, 2*time.Second, 2*EventTimeout)
 			if err != nil {
@@ -167,6 +178,7 @@ func (d *Device) StartEventChannel() error {
 				case 200:
 					// OK, there is a new event
 					Etag = resp.Header.Get("Etag")
+					TRACE.Println("Got Etag", Etag)
 					ByteArray, err := ioutil.ReadAll(resp.Body)
 					if err != nil {
 						d.SetError(DeviceError("Device.StartEventChannel.goroutine", "ReadAll", err))
@@ -183,11 +195,12 @@ func (d *Device) StartEventChannel() error {
 				default: // Other return codes denote a problem
 					d.SetError(DeviceError("Device.StartEventChannel.goroutine", "Unexpected status"+resp.Status, err))
 				}
+
 			}
 		}
 	}()
 
-	return nil
+	return closeme, nil
 }
 
 func (d *Device) ParseEventTable(HPEventTable *HPEventTable) (err error) {
@@ -301,14 +314,16 @@ func (d *Device) GetWalkupScanToCompDestinations(uri string) (*HPWalkupScanToCom
 	if err != nil {
 		return nil, DeviceError("Device.WalkupScanToCompDestinations", "", err)
 	}
-	defer resp.Body.Close()
+	resp.Body.Close()
 
 	if resp.StatusCode != 200 {
+		resp.Body.Close()
 		return nil, DeviceError("Device.WalkupScanToCompDestinations", "Unexpected Status "+resp.Status, err)
 	}
 
 	s, err := ioutilReadAll(resp.Body)
 	if err != nil {
+		resp.Body.Close()
 		return nil, err
 	}
 	dest := new(HPWalkupScanToCompDestination)
@@ -320,16 +335,18 @@ func (d *Device) GetWalkupScanToCompDestinations(uri string) (*HPWalkupScanToCom
 	if err != nil {
 		return nil, DeviceError("Device.WalkupScanToCompDestinations", "", err)
 	}
-	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
+		resp.Body.Close()
 		return nil, DeviceError("Device.WalkupScanToCompDestinations", "Unexpected Status "+resp.Status, err)
 	}
 
 	s, err = ioutilReadAll(resp.Body)
 	if err != nil {
+		resp.Body.Close()
 		return nil, err
 	}
+	resp.Body.Close()
 	dest = new(HPWalkupScanToCompDestination)
 	err = xml.Unmarshal(s, dest)
 	return dest, err
@@ -343,8 +360,8 @@ func (d *Device) Register(settings MapOfDestinationSettings) (err error) {
 	if err != nil {
 		return DeviceError("Register", "DiscoveryTree", err)
 	}
-	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
+		resp.Body.Close()
 		return DeviceError("Register", "Unexpected Status "+resp.Status, err)
 	}
 
@@ -360,6 +377,7 @@ func (d *Device) Register(settings MapOfDestinationSettings) (err error) {
 		return DeviceError("Register", "WalkupScanToCompDestinations", err)
 	}
 	if resp.StatusCode != 200 {
+		resp.Body.Close()
 		return DeviceError("Register", "Unexpected Status "+resp.Status, err)
 	}
 
@@ -367,6 +385,7 @@ func (d *Device) Register(settings MapOfDestinationSettings) (err error) {
 	s, err := ioutil.ReadAll(resp.Body)
 	CheckError("ReadBody", err)
 	if err = xml.Unmarshal(s, destinations); err != nil {
+		resp.Body.Close()
 		return err
 	}
 	resp.Body.Close()
@@ -388,7 +407,6 @@ func (d *Device) Register(settings MapOfDestinationSettings) (err error) {
 		if resp, err = HttpPost(d.URL+"/WalkupScanToComp/WalkupScanToCompDestinations", "text/xml", r); err != nil {
 			return DeviceError("Register / Post WalkupScanToCompDestinations", "HTTP", err)
 		}
-		defer resp.Body.Close()
 		if resp.StatusCode != 201 {
 			return DeviceError("Register /  Post WalkupScanToCompDestinations", "Unexpected Status "+resp.Status, err)
 		}
@@ -404,6 +422,7 @@ func (d *Device) Register(settings MapOfDestinationSettings) (err error) {
 			URI:                 uri,
 		}
 		INFO.Println("Registration successful:", Me.Hostname, uuid)
+		resp.Body.Close()
 	}
 	return err
 }
